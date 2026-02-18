e9a7e376dd989e3da848fd942d417ca419e2d7a5
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-02-18 11:39:31 -0300

    Create index.js

---
+++ Aula02_Variaveis/index.js
@@ -0,0 +1,26 @@
+let x; // declarando variavel
+
+x = 200; // dando valor a variavel
+
+console.log(x);
+
+let idade = 19;
+let preco = 10.99;
+let nome = "Joao";
+let cpf = "979-969-382-12";
+
+console.log(`Bom dia! Seu nome é ${nome}, seu CPF é ${cpf}
+e sua idade é ${idade}, dito isso voce terá que pagar R$${preco} pela pizza que comprou`);
+
+console.log(typeof idade);
+console.log(typeof preco);
+console.log(typeof nome);
+console.log(typeof cpf);
+
+let bool = true; // ou falso
+
+let online = false;
+
+console.log(`Joao está Online: ${online}`);
+
+window.alert(`JOAO ONLINE: ${online}`);
