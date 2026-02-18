2f9bdc9ac42a60887f6510881d02b38790982815
Rafael Borges <rafaborgesdasilva75@gmail.com>
2026-02-18 10:59:09 -0300

    Add initial script.js with console logs and DOM updates

---
+++ Aula_01/script.js
@@ -0,0 +1,9 @@
+console.log("Olá");
+console.log("Sou uma linha do console");
+
+//window.alert("Isso é um,");
+//window.alert("Alerta");
+
+// adicionam texto ao elemento do ID vindo do HTML dado nos parenteses
+document.getElementById("01").textContent = "Ola!";
+document.getElementById("p01").textContent = "dawdasdw";
